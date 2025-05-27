04b528931a8f81f32d871396bb62cd31f5f526c9
Mimmi Eriksson <mimmi.aj.eriksson@gmail.com>
2025-05-27 09:02:17 +0200

    responsiveness fix

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -6,7 +6,7 @@ import useTaskStore from "../stores/useTaskStore"
 const TaskList = () => {
   const tasks = useTaskStore(state => state.tasks)
   return (
-    <div className="bg-white dark:bg-background-dark border-x-1 border-border dark:border-border-dark w-full flex-grow flex flex-col max-w-2xl">
+    <div className="bg-white dark:bg-background-dark border-x-1 border-border dark:border-border-dark w-full flex-grow flex flex-col max-w-[1024px]">
       {tasks.length > 0 && (
         <div className="flex flex-col">
           {tasks.map(task => <Task key={task.id} task={task} />)}
